ef30aa38c73b16d91275d011c89709676a8bc6a2
JannatN <jannatfaisalnatsheh@gmail.com>
2022-09-15 14:57:04 +0300

    Generate new images when click on button

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,29 @@ import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
+
   componentDidMount() {
     fetch("https://picsum.photos/v2/list")
       .then(response => response.json())
       .then(data => {
-        this.setState({ images: data });
+        this.setState({
+          images: data, showComponent: true,
+        });
         // console.log(this.state)
 
       });
+
   }
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
-        <ImageList images={this.state.images.slice(0, 6)} />
-        <Button images={this.state.images} ></Button>
+        {this.state.showComponent ?
+          <ImageList images={this.state.images.slice(0, 6)} /> :
+          null
+        }
+        {/* <ImageList images={this.state.images.slice(0, 6)} /> */}
+        <Button images={this.state.images}  ></Button>
 
       </div>
     );
